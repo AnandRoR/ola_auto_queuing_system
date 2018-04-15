d2593bd823aa626c49561543c856a74d13554cba
Anand <jha.anand529@gmail.com>
2018-04-15 17:02:46 +0530

    added request form and other views as well

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,6 +10,7 @@ class CustomersController < ApplicationController
   # GET /customers/1
   # GET /customers/1.json
   def show
+    @request =  Request.new
   end
 
   # GET /customers/new
@@ -69,6 +70,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.fetch(:customer, {})
+      params.require(:customer).permit(:first_name, :last_name)
     end
 end
